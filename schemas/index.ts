868f46dd7cf0fdee189695c1cd3cfe4cc1203379
OmarMo7 <omar7.first@gmail.com>
2024-06-01 23:14:19 +0300

    Building the architecure of our database in Sanity

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import about from './about'
+import brand from './brands'
+import contact from './contact'
+import work from './works'
+import experiences from './experiences'
+import skills from './skills'
+import work_experiences from './work_experiences'
+
+export const schemaTypes = [
+  testimonials,
+  about,
+  brand,
+  contact,
+  work,
+  experiences,
+  skills,
+  work_experiences,
+]
