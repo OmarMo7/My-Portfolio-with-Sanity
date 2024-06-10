c3b4aa8c01fdc145c705147e76baeb8a8265810a
OmarMo7 <omar7.first@gmail.com>
2024-06-10 15:50:43 +0200

    Adding "python" tag

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -38,7 +38,7 @@ const Work = () => {
     <>
       <h2 className='head-text'>My creative <span>Portfolio</span></h2>
       <div className='app__work-filter'>
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'Python','All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
